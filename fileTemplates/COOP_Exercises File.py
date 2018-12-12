b78f758bb6804aff8cd39a03ce83965ce1d69b2d
Michael <43071900+michaelh21@users.noreply.github.com>
2018-12-12 11:56:07 -0600

    PY-2018.3.1 <micha@LAPTOP-7C0CIMBF Create ide.general.xml, git.xml, project.default.xml, laf.xml, material_custom_theme.xml, other.xml, jdk.table.xml, Default.xml, filetypes.xml, material_theme.xml, COOP_Exercises File.py, vcs.xml, editor.codeinsight.xml, colors.scheme.xml, _@user_Darcula.icls, github.xml, debugger.xml, updates.xml

---
+++ fileTemplates/COOP_Exercises File.py
@@ -0,0 +1,21 @@
+# ${NAME}.py
+# 
+# Author: Michael Hu
+# Course: Coding for OOP
+# Section: A3
+# Date: ${DAY} ${MONTH_NAME_SHORT} ${YEAR}
+# IDE: #{PRODUCT_NAME}
+# 
+# Assignement Info
+#   Exercise: 
+#   Source: Python Programming 
+#   Chapter: 
+#
+# Program Description
+# 
+#
+#
+# Algorithm (pseudocode)
+#
+#
+#
