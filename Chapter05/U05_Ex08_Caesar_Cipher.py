3cc7def53269062fc5e37e943efc3b9b46cff212
Michael Hu <michael.h21@student.parishepiscopal.org>
2018-12-17 13:25:42 -0600

    Algorithm

---
+++ Chapter05/U05_Ex08_Caesar_Cipher.py
@@ -0,0 +1,31 @@
+# U05_Ex08_Caesar_Cipher.py.py
+# 
+# Author: Michael Hu
+# Course: Coding for OOP
+# Section: A3
+# Date: 07 Dec 2018
+# IDE: #{PRODUCT_NAME}
+# 
+# Assignement Info
+#   Exercise: 8
+#   Source: Python Programming 
+#   Chapter: 05
+#
+# Program Description
+# This program allows the user to encode a line of text using a key value and a line of text. The encoding method
+# is the Caesar cipher.
+#
+#
+# Algorithm (pseudocode)
+# Get the input for the key value
+# Get the input for the phrase they want to encode
+# Make the phrase lowercase
+# I will translate the letters into numbers, adding the key value to the numbers, and converting them back into letters
+# I can use an if statement before I convert the numbers back into letters to say if a number is over 26, we -25
+#
+
+def main():
+
+
+
+main()
