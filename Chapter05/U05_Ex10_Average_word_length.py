05a76cf8ccb4342201a973be278c2c32cc3204b6
Michael Hu <michael.h21@student.parishepiscopal.org>
2018-12-17 18:28:24 -0600

    5.9 Finished

---
+++ Chapter05/U05_Ex10_Average_word_length.py
@@ -0,0 +1,21 @@
+# U05_Ex10_Average_word_length.py.py
+# 
+# Author: Michael Hu
+# Course: Coding for OOP
+# Section: A3
+# Date: 07 Dec 2018
+# IDE: #{PRODUCT_NAME}
+# 
+# Assignement Info
+#   Exercise: 10
+#   Source: Python Programming 
+#   Chapter: 05
+#
+# Program Description
+# This program gives the average word length in a sentence of user input.
+#
+#
+# Algorithm (pseudocode)
+#
+#
+#
---
+++ Chapter05/U05_Ex12_Future_value.py
@@ -0,0 +1,21 @@
+# U05_Ex12_Future_value.py.py
+# 
+# Author: Michael Hu
+# Course: Coding for OOP
+# Section: A3
+# Date: 07 Dec 2018
+# IDE: #{PRODUCT_NAME}
+# 
+# Assignement Info
+#   Exercise: 12
+#   Source: Python Programming 
+#   Chapter: 05
+#
+# Program Description
+# This program alters the futval.py program and outputs a formatted table that displays each year in the investment.
+#
+#
+# Algorithm (pseudocode)
+#
+#
+#
