57f1a6ed20624f788e1e6b177d3b685deadccdc4
Michael Hu <michael.h21@student.parishepiscopal.org>
2019-01-23 10:21:32 -0600

    Issues fixed

--- Chapter05/U05_Ex08_Caesar_Cipher1.py
+++ Chapter05/U05_Ex08_Caesar_Cipher1.py
@@ -20,8 +20,12 @@
 # Get the input for the key value
 # Get the input for the phrase they want to encode
 # Make the phrase lowercase
+# To solve the letters not looping around like y to a, I can copy and paste the alphabet in the same list so that
+# anything that goes overboard will do a hard loop
 # I will translate the letters into numbers, adding the key value to the numbers, and converting them back into letters
 # I can use an if statement before I convert the numbers back into letters to say if a number is over 26, we -25
+# Letter by letter, I can translate characters into numbers, add to those numbers from the key value the user inputs,
+# and convert the numbers back to characters using the list.
 #
 
 def main():
@@ -42,7 +46,8 @@ def main():
     fill = ""
 
     letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
-               "v", "w", "x", "y", "z"]
+               "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+               "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
     for char in word:
         charNum = ord(char)-97+key
