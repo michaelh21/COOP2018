19f916eea3f5beeee20f2de29b8435909d36034f
Michael Hu <michael.h21@student.parishepiscopal.org>
2019-02-25 10:26:17 -0600

    6.6 Fixed

--- Chapter06/U06_Ex06_AOT.py
+++ Chapter06/U06_Ex06_AOT.py
@@ -16,36 +16,62 @@
 #
 #
 # Algorithm (pseudocode)
-# Copy paste exercise 3.9
-# Make the input into different functions
-# Make the calculation into different function
-# Make main the print statement in main
-# For the calculation and main, I will need to pull the variables from the other functions; done with return statement
+# Copy paste exercise 4.10
+# Make it so that on each click, the program draws a dot.
+#   I can copy paste some point drawing code from unit 4
 
+from graphics import *
+from Chapter04.U04_Ex02_Archery import makeCircle
 import math
 
-def input1():
-    side1 = eval(input("Enter side one: "))
-    return side1
-
-def input2():
-    side2 = eval(input("Enter side two: "))
-    return side2
-
-def input3():
-    side3 = eval(input("Enter side three: "))
-    return side3
-
-def calc():
-    side1 = input1()
-    side2 = input2()
-    side3 = input3()
-    s = (side1 + side2 + side3) / 2
-    area = math.sqrt(s * (s - side1) * (s - side2) * (s - side3))
-    return area
-
 def main():
-    area = calc()
-    print("The triangle's area is {0}".format(area))
+    win = GraphWin("RectangleInfo", 400, 400)
+    program(win)
+
+
+def program(win):
+    p = win.getMouse()
+    x1 = p.getX()
+    y1 = p.getY()
+    makeCircle(Point(x1, y1), 5, "black").draw(win)
+
+    g = win.getMouse()
+    x2 = g.getX()
+    y2 = g.getY()
+    makeCircle(Point(x2, y2), 5, "black").draw(win)
+
+    q = win.getMouse()
+    x3 = q.getX()
+    y3 = q.getY()
+    makeCircle(Point(x3, y3), 5, "black").draw(win)
+
+    shape = Polygon(Point(x1, y1), Point(x2, y2), Point(x3, y3))
+    shape.setOutline("black")
+    shape.setFill("white")
+    shape.draw(win)
+
+    difx = abs(x2 - x1)
+    dify = abs(y2 - y1)
+    difx2 = abs(x3 - x2)
+    dify2 = abs(y3 - y2)
+    difx3 = abs(x3 - x1)
+    dify3 = abs(y3 - y1)
+    length1 = round(math.sqrt(difx**2 + dify**2), 1)
+    length2 = round(math.sqrt(difx2**2 + dify2**2), 1)
+    length3 = round(math.sqrt(difx3**2 + dify3**2), 1)
+
+    perimeter = round((length1+length2+length3), 2)
+    Text(Point(50, 20), "Perimeter:").draw(win)
+    Text(Point(130, 20), perimeter).draw(win)
+
+    s = perimeter / 2
+    Area = round((math.sqrt(s*(s-length1)*(s-length2)*(s-length3))), 2)
+    Text(Point(50, 50), "Area:").draw(win)
+    Text(Point(130, 50), Area).draw(win)
+
+    win.getMouse()
+    win.close()
+
+
 main()
 
